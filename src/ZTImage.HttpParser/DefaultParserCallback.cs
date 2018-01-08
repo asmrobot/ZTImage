9cd121bd637920bef998352b90f9a6765593c02a
asmrobot <asmrobot@hotmail.com>
2018-01-08 16:33:04 +0800

    init

--- src/ZTImage.HttpParser/DefaultParserCallback.cs
+++ src/ZTImage.HttpParser/DefaultParserCallback.cs
@@ -10,53 +10,63 @@ namespace ZTImage.HttpParser
     {
         public unsafe int on_body(HttpFrame frame, byte* data)
         {
+            Console.WriteLine("on_body");
             return 0;
         }
 
         public int on_chunk_complete(HttpFrame frame)
         {
+            Console.WriteLine("on_chunk_complete");
             return 0;
         }
 
         public int on_chunk_header(HttpFrame frame)
         {
+            Console.WriteLine("on_chunk_header");
             return 0;
         }
 
         public int on_headers_complete(HttpFrame frame)
         {
+            Console.WriteLine("on_headers_complete");
             return 0;
         }
 
         public unsafe int on_header_field(HttpFrame frame, byte* data)
         {
+            Console.WriteLine("on_header_field");
             return 0;
         }
 
         public unsafe int on_header_value(HttpFrame frame, byte* data)
         {
+            Console.WriteLine("on_header_value");
             return 0;
         }
 
         public int on_message_begin(HttpFrame frame)
         {
+            Console.WriteLine("on_message_begin");
             return 0;
         }
 
         public int on_message_complete(HttpFrame frame)
         {
+            Console.WriteLine("on_message_complete");
             return 0;
         }
         
 
         public unsafe int on_status(HttpFrame frame, byte* data)
         {
+            Console.WriteLine("on_status");
             return 0;
         }
         
 
         public unsafe int on_url(HttpFrame frame, byte* data)
         {
+            Console.WriteLine("on_url");
             return 0;
         }
     }
