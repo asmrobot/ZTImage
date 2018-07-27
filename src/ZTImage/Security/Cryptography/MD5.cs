bda977cd82114861bde08da75742e5c8f2d569fd
asmrobot <asmrobot@hotmail.com>
2018-07-27 14:26:58 +0800

    md5 add functions

--- src/ZTImage/Security/Cryptography/MD5.cs
+++ src/ZTImage/Security/Cryptography/MD5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -41,5 +42,55 @@ namespace ZTImage.Security.Cryptography
             ret = ret.ToLower();
             return ret;
         }
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="bytedata"></param>
+        /// <returns></returns>
+        public static string Encrypt(byte[] bytedata)
+        {
+            try
+            {
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(bytedata);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5Hash() fail,error:" + ex.Message);
+            }
+        }
+
+        public static string EncryptFile(string fileName)
+        {
+            try
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5Hash() fail,error:" + ex.Message);
+            }
+        }
+
+       
+
     }
 }
