32470e7dd5c23156aa6f2708720ed4a8c092da97
asmrobot <asmrobot@hotmail.com>
2018-08-01 17:41:01 +0800

    add sha256

---
+++ src/ZTImage/Security/Cryptography/SHA256.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ZTImage.Security.Cryptography
+{
+
+    /// <summary>
+    /// SHA1加密
+    /// 默认utf-8
+    /// </summary>
+    public class SHA256
+    {
+        public string Encrypt(string str)
+        {
+            return Encrypt(str, Encoding.ASCII);
+
+        }
+        
+        public string Encrypt(string str, Encoding encoding)
+        {
+            return Encrypt(encoding.GetBytes(str));
+        }
+
+        public string Encrypt(byte[] bytes)
+        {
+            byte[] tmpByte;
+            SHA256Managed sha256 = new SHA256Managed();
+            tmpByte = sha256.ComputeHash(bytes);
+            sha256.Clear();
+            return System.Text.Encoding.ASCII.GetString(tmpByte);
+        }
+
+    }
+}
